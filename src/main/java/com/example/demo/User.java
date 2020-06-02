5cd80c769400f6b62929487b8c9afd71d8b4baa3
PH <gnatenko.pavlo@gmail.com>
2020-06-02 18:29:59 +0300

    start

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    public long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody User user) {
+        userService.create(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity update(@RequestBody User user) {
+        userService.update(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(Long id) {
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+
+}
